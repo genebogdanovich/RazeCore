f2d9d95a8b06ab1dec7b936e58d8b4aa16ec6c6f
ProgressGuard <help@progressguard.com>
2022-05-16 10:51:39 +0300

    Add Networking class.

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,14 @@
+//
+//  Networking.swift
+//  RazeCore
+//
+//  Created by Gene Bogdanovich on 16.05.22.
+//
+
+import Foundation
+
+extension RazeCore {
+    public class Networking {
+        
+    }
+}
---
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -0,0 +1,13 @@
+//
+//  RazeNetworkingTests.swift
+//  RazeCoreTests
+//
+//  Created by Gene Bogdanovich on 16.05.22.
+//
+
+import XCTest
+@testable import RazeCore
+
+final class RazeNetworkingTests: XCTestCase {
+    
+}
