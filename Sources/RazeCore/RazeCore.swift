34d5e0d36626aa9ab4c7f8c86539c7fb83a8af5a
ProgressGuard <help@progressguard.com>
2022-05-16 10:30:25 +0300

    Add secondary color as static variable.

--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -33,5 +33,12 @@ public class RazeCore {
     public static var razeColor: UIColor {
         return self.colorFromHexString("006736")
     }
+    
+    /// Returns an object of `UIColor` with the second most eye-pleasing color known to all humanity.
+    public static var secondaryRazeColor: UIColor {
+        return self.colorFromHexString("FCFFFD")
+    }
+    
+    
 }
 
