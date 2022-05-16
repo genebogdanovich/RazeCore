d7a16a81abcc93ca95a9820fbeea1120da71dec7
ProgressGuard <help@progressguard.com>
2022-05-16 11:19:56 +0300

    Add first networking API for retrieving network data.

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -9,5 +9,22 @@ import XCTest
 @testable import RazeCore
 
 final class RazeNetworkingTests: XCTestCase {
-    
+    func testLoadDataCall() {
+        let manager = RazeCore.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data.")
+        guard let url = URL(string: "https://raywenderlich.com") else {
+            return XCTFail("Could not create URL properly")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
 }
